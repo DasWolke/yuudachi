e0e130f9b25c2d178e3a8bd97bd5425d90275ed7
daswolke <wolke@weeb.sh>
2018-01-03 20:14:05 +0100

    add username to recommendation leaderboard

--- commands/community/recommendation/RecommendationLeaderboard.js
+++ commands/community/recommendation/RecommendationLeaderboard.js
@@ -19,23 +19,23 @@ class RecommendationLeaderboard extends SubCommand {
 
         let embed = {
             title: '<:poiHappy:379720490924769292> Most recommended Admirals <:poiHappy:379720490924769292>',
-            fields: this._prepareLeaderboardFields(leaderboard)
+            fields: await this._prepareLeaderboardFields(leaderboard)
         };
         return this.bot.rest.channel.createMessage(msg.channel_id, {embed});
     }
 
-    _prepareLeaderboardFields(leaderboard) {
+    async _prepareLeaderboardFields(leaderboard) {
         let i = 0;
         let x = 0;
         let fields = [];
         for (let user of leaderboard) {
             if ((i === 0 && x === 0) || (x % 3 === 0 && x > 0)) {
-                fields.push({name: `${x + 1}-${x + 3}`, value: this._prepareLeaderboardName(user, x)});
+                fields.push({name: `${x + 1}-${x + 3}`, value: await this._prepareLeaderboardName(user, x)});
                 if (x !== 0) {
                     i++;
                 }
             } else {
-                fields[i].value += this._prepareLeaderboardName(user, x);
+                fields[i].value += await this._prepareLeaderboardName(user, x);
             }
             x++;
         }
@@ -46,9 +46,17 @@ class RecommendationLeaderboard extends SubCommand {
         return this.bot.handler.weebHandler.loadReputationLeaderboard(botId);
     }
 
-    _prepareLeaderboardName(user, x) {
+    async _prepareLeaderboardName(user, x) {
         let trophy = x === 0 ? ' :trophy:' : x === 1 ? ':second_place:' : x === 2 ? ':third_place:' : '';
-        return `**${x + 1}.**${trophy} <@${user.userId}> (${user.reputation})\n`;
+        return `**${x + 1}.**${trophy} <@${user.userId}> (${await this._getUsernameFromId(user.userId)}) (${user.reputation})\n`;
+    }
+
+    async _getUsernameFromId(id) {
+        let user = await this.bot.cache.user.get(id);
+        if (!user) {
+            return 'User not found';
+        }
+        return user.username;
     }
 }
 
